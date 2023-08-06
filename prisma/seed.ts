c79fd23b625baa59ce787ba3f1dbca97eb949109
sagarPakhrin <sagarlama826@gmail.com>
2023-08-06 08:50:00 +0545

    fixup! add rating docs

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,19 +1,18 @@
 // Require the cloudinary library
-import { faker } from '@faker-js/faker';
 import { PrismaClient } from '@prisma/client';
 
 const prisma = new PrismaClient();
 async function main() {
-  for (let i = 0; i < 18; i++) {
-    await prisma.product.create({
-      data: {
-        name: faker.commerce.productName(),
-        price: parseFloat(faker.commerce.price()),
-        image: 'image',
-        category_id: faker.datatype.number({ min: 1, max: 4 }),
-      },
-    });
-  }
+  // for (let i = 0; i < 18; i++) {
+  //   await prisma.product.create({
+  //     data: {
+  //       name: faker.commerce.productName(),
+  //       price: parseFloat(faker.commerce.price()),
+  //       image: 'image',
+  //       category_id: faker.datatype.number({ min: 1, max: 4 }),
+  //     },
+  //   });
+  // }
 }
 main()
   .then(async () => {
